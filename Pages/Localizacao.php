9eef2e14a77cf1f182307123603565b0561ad942
MatosGabrielSBS <89490258+MatosGabrielSBS@users.noreply.github.com>
2021-08-27 21:00:37 -0300

    Vai ser atualizado endereços das paginas

--- Pages/Localizacao.php
+++
@@ -1,3 +0,0 @@
-<h2>Nossa Localização</h2>
-<p>Rua Carlos Bayerl, 200 - Progresso, São Bento do Sul - SC, 89281-066</p>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1789.2889394842582!2d-49.370357528272905!3d-26.242901997069154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ddd4b425427a55%3A0xc018d263eb7297f!2sRua%20Carlos%20Bayerl%2C%20200%20-%20Progresso%2C%20S%C3%A3o%20Bento%20do%20Sul%20-%20SC%2C%2089281-066!5e0!3m2!1spt-BR!2sbr!4v1630027647199!5m2!1spt-BR!2sbr" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
